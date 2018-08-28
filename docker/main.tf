8cc65a591ad4590f3037f86bf72badd248921f3a
Dawit Nida <dawit.nida@supermetrics.com>
2018-08-28 14:01:51 +0300

    Updated docker terraform configs

--- docker/main.tf
+++ docker/main.tf
@@ -1,19 +1,25 @@
 # Configure Docker provider and connect to the local Docker socket
+terraform {
+  backend "s3" {}
+}
+
 provider "docker" {
-  host = "unix:///var/run/docker.sock"
+  host = "${var.docker_host}"
 }
 
 # Create a container
 resource "docker_container" "docker_dev_nginx" {
   image = "${docker_image.nginx.latest}"
-  name  = "docker-dev-nginx"
+  name  = "dev-nginx-doc"
 
   ports {
-    internal = 80
-    external = 80
+    internal = 8082
+    external = 8082
   }
+
+  restart = "no"
 }
 
 resource "docker_image" "nginx" {
-  name = "nginx:latest"
+  name = "${var.image}"
 }
